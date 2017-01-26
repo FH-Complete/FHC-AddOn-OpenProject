618ae971539ad440a3be078ac6704e4803c1429e
Sebastian Koller <seb.koller@gmail.com>
2017-01-26 15:07:42 +0100

    add rest lib in FHCOP Model + convert to unix file breaks

--- vilesci/application/core/FHCOP_Model.php
+++ vilesci/application/core/FHCOP_Model.php
@@ -4,9 +4,25 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class FHCOP_Model extends CI_Model {
 
-    public $result;
+	public $result;
 
-    function __construct() {
-        parent::__construct();
-    }
+	function __construct() {
+		parent::__construct();
+
+		$this->load->library('rest');
+
+		$config = $this->config->item('openproject');
+		$url = rtrim($config['server'], '/') . $config['api_path'];
+
+		$rest_config = [
+			'server' => $url,
+			'http_user' => 'apikey',
+			'http_pass' => $config['api_key'],
+			'http_auth' => 'basic',
+			//'ssl_verify_peer' => TRUE,
+			//'ssl_cainfo' => '/certs/cert.pem'
+		];
+
+		$this->rest->initialize($rest_config);
+	}
 }
