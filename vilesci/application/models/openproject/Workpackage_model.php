3ce98d724a3648d2168b16f68bd67fd540cf616e
Sebastian Koller <seb.koller@gmail.com>
2017-01-30 21:50:21 +0100

    replace remaining tabs with spaces [PART 2]

--- vilesci/application/models/openproject/Workpackage_model.php
+++ vilesci/application/models/openproject/Workpackage_model.php
@@ -10,55 +10,55 @@ class Workpackage_model extends FHCOP_Model
     /**
      * Workpackage_model constructor.
      */
-	public function __construct()
-	{
-		parent::__construct();
-	}
-
-
-	/**
-	 * Create a work package.
-	 *
-	 * Creates a work package via the Open Project API.
-	 *
-	 * @param Workpackage $wp the work package object to be persisted
-	 * @return int|null The id of the created work package or null if it fails.
-	 */
-	public function create($wp)
+    public function __construct()
     {
-		$data = [
-			'subject' => $wp->subject,
-			'description' => [
-				'raw' => $wp->description,
-			],
-			'_links' => [
-				'type' => [
-					'href' => $wp->typeResource
-				],
-				'responsible' => [
-					'href' => $wp->responsibleResource
-				],
-				'status' => [
-					'href' => $wp->statusResource
-				],
-				'parent' => [
-					'href' => $wp->parentResource
-				],
-			],
-			'startDate' => $wp->startDate,
-			'dueDate' => $wp->dueDate,
-			'estimatedTime' => $wp->estimatedTime,
-		];
+        parent::__construct();
+    }
 
-		$result = $this->rest->postJSON("projects/$wp->projectID/work_packages", $data);
 
-		if (!property_exists($result, 'id'))
-		{
-			print_r($result);
-		}
+    /**
+     * Create a work package.
+     *
+     * Creates a work package via the Open Project API.
+     *
+     * @param Workpackage $wp the work package object to be persisted
+     * @return int|null The id of the created work package or null if it fails.
+     */
+    public function create($wp)
+    {
+        $data = [
+            'subject' => $wp->subject,
+            'description' => [
+                'raw' => $wp->description,
+            ],
+            '_links' => [
+                'type' => [
+                    'href' => $wp->typeResource
+                ],
+                'responsible' => [
+                    'href' => $wp->responsibleResource
+                ],
+                'status' => [
+                    'href' => $wp->statusResource
+                ],
+                'parent' => [
+                    'href' => $wp->parentResource
+                ],
+            ],
+            'startDate' => $wp->startDate,
+            'dueDate' => $wp->dueDate,
+            'estimatedTime' => $wp->estimatedTime,
+        ];
 
-		return property_exists($result, 'id') ? $result->id : null;
-	}
+        $result = $this->rest->postJSON("projects/$wp->projectID/work_packages", $data);
+
+        if (!property_exists($result, 'id'))
+        {
+            print_r($result);
+        }
+
+        return property_exists($result, 'id') ? $result->id : null;
+    }
 
     /**
      * Sets the parent work package of a work package
@@ -66,26 +66,26 @@ class Workpackage_model extends FHCOP_Model
      * @param int $id work package to set parent for
      * @param int $parentID parent work package
      */
-	public function set_parent($id, $parentID)
-	{
-		$workPackage = $this->rest->get("work_packages/{$id}");
+    public function set_parent($id, $parentID)
+    {
+        $workPackage = $this->rest->get("work_packages/{$id}");
 
-		if (!property_exists($workPackage, 'id'))
-		{
-			// work package does not exists
-			return;
-		}
+        if (!property_exists($workPackage, 'id'))
+        {
+            // work package does not exists
+            return;
+        }
 
-		$api_path = $this->config->item('openproject')['api_path'];
+        $api_path = $this->config->item('openproject')['api_path'];
 
-		$data = [
-			"lockVersion" => $workPackage->lockVersion,
-			"_links" => [
-				"parent" => [
-					"href" => "{$api_path}work_packages/{$parentID}"
-				]
-			]
-		];
-		$this->rest->patch("work_packages/{$id}", json_encode($data), 'json');
-	}
+        $data = [
+            "lockVersion" => $workPackage->lockVersion,
+            "_links" => [
+                "parent" => [
+                    "href" => "{$api_path}work_packages/{$parentID}"
+                ]
+            ]
+        ];
+        $this->rest->patch("work_packages/{$id}", json_encode($data), 'json');
+    }
 }
