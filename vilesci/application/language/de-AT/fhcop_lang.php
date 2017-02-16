4a723c85f5a48ec927b390c87d7374ce9322099b
Philipp Schwarzkopf <phisi93@hotmail.com>
2017-02-16 17:10:08 +0100

    Changed language location

--- vilesci/application/language/de-AT/fhcop_lang.php
+++ vilesci/application/language/de-AT/fhcop_lang.php
@@ -5,3 +5,4 @@ $lang['fhcop_'.FHCOP_NOT_FOUND]	    = 'Projekt mit dieser projekt_kurzbz konnte 
 $lang['fhcop_'.FHCOP_ACCESS_DENIED]	= 'Sie brauchen die nötige Berechtigung für diese Seite';
 $lang['fhcop_'.FHCOP_MISSING_USERS]	= 'Nicht alle Benutzer sind in OpenProject vorhanden. Durch nutzen des GET Parameter \'user_check=false\' kann dieser Fehler ignoriert werden';
 $lang['fhcop_'.FHCOP_CANNOT_CREATE]	= 'Fehler beim erstellen des Projekts, es existiert eventuell bereits';
+$lang['fhcop_'.FHCOP_NO_CONFIG] 	= 'Es ist keine Config gesetzt, folge dem angegebenen Pfad um dies zu lösen';
